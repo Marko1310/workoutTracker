98124191452c8118e9517aecb594e223f3e62cf7
Marko1310 <marko.cabo13@gmail.com>
2023-04-24 14:05:08 +0200

    split grid style

--- workout_tracker/root/frontend/src/components/Splits/SplitGrid.js
+++ workout_tracker/root/frontend/src/components/Splits/SplitGrid.js
@@ -1,24 +1,25 @@
 // React
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from 'react';
 
 // Components
-import AddSplitBtn from "./AddSplitBtn";
-import NewSplit from "./NewSplitModal.js";
+import AddSplitBtn from './AddSplitBtn';
+import NewSplit from './NewSplitModal.js';
+import HelpModal from '../HelpModal/HelpModal';
 
 // Components
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 // Context
-import { GlobalContext } from "../../context/GlobalContext.js";
+import { GlobalContext } from '../../context/GlobalContext.js';
 
 // css
-import "./SplitGrid.css";
+import './SplitGrid.css';
 
 // image
-import calendar from "../../images/calendar.png";
+import calendar from '../../images/calendar.png';
 
 const WorkoutSplitGrid = () => {
-  const { isModalOpen } = useContext(GlobalContext);
+  const { isModalOpen, isMenuOpen } = useContext(GlobalContext);
   const { user } = useContext(GlobalContext);
   const { splits } = useContext(GlobalContext);
   const { getWorkouts } = useContext(GlobalContext);
@@ -26,14 +27,23 @@ const WorkoutSplitGrid = () => {
   const { deleteSplit } = useContext(GlobalContext);
   const { setLoadingTimeout } = useContext(GlobalContext);
 
+  // state
+  const [helpModalOpen, setHelpModalOpen] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) {
-      navigate("/");
+      navigate('/');
     }
     getSplits();
   }, [user, navigate]);
+
+  useEffect(() => {
+    if (splits.length === 0) {
+      setHelpModalOpen(true);
+    } else setHelpModalOpen(false);
+  }, [splits]);
 
   const changeRoute = (id) => {
     getWorkouts(id);
@@ -41,7 +51,7 @@ const WorkoutSplitGrid = () => {
   };
 
   const handleDelete = (e, split_id) => {
-    if (window.confirm("Are you sure you want to delete this Workout Split?")) {
+    if (window.confirm('Are you sure you want to delete this Workout Split?')) {
       deleteSplit(e, split_id);
       setLoadingTimeout();
     }
@@ -50,54 +60,92 @@ const WorkoutSplitGrid = () => {
 
   return (
     <>
-      <div className="main-container">
-        <div className={`${isModalOpen ? "blurred" : ""}`}>
-          <p className="choose">Choose a Workout Split</p>
-          <div className="workout-grid">
-            {splits.length > 0 &&
-              splits.map((el) => {
-                return (
-                  <ul
-                    key={el.split_id}
-                    onClick={
-                      isModalOpen ? null : () => changeRoute(el.split_id)
-                    }
-                    className="workout-container"
-                  >
-                    <div className="image-and-delete-container">
-                      <img
-                        className="workout-image"
-                        src={calendar}
-                        alt="Workout"
-                      ></img>
-                      <button
-                        onClick={(e) => handleDelete(e, el.split_id)}
-                        className="delete-split"
-                      >
-                        Delete
+      {helpModalOpen ? (
+        <HelpModal message={'splits'} />
+      ) : (
+        <div className="main-container">
+          <div className={`${isModalOpen || isMenuOpen ? 'blurred' : ''}`}>
+            <p className="choose-title">Choose your Workout Split:</p>
+            <div className="workout-grid">
+              {splits.length > 0 &&
+                splits.map((el) => {
+                  return (
+                    <ul key={el.split_id} className="workout-container">
+                      <div className="image-and-delete-container">
+                        <img className="workout-image" src={calendar} alt="Workout"></img>
+                        <p onClick={(e) => handleDelete(e, el.split_id)} className="delete-split">
+                          Delete
+                        </p>
+                      </div>
+                      <div className="workout-card">
+                        <li className="workout-card-title">{el.split_name} workout</li>
+                        <li className="workout-card-workouts-days">{el.days} day split:</li>
+                        {el.array_agg.map((name, index) => {
+                          return (
+                            <li className="workout-card-workouts">
+                              - Day {index + 1} : {name} day
+                            </li>
+                          );
+                        })}
+                      </div>
+                      <button className="enter-split" onClick={() => changeRoute(el.split_id)}>
+                        Choose Split
                       </button>
-                    </div>
-                    <div className="workout-card">
-                      <li className="workout-card-title">{el.split_name}</li>
-                      <li>{el.days} day split:</li>
-                      {el.array_agg.map((name) => {
-                        return <li> - {name} day</li>;
-                      })}
-                      <li>--------------------------------</li>
-                      <li>Created on: {el.date.slice(0, 10)}</li>
-                    </div>
-                  </ul>
-                );
-              })}
+                    </ul>
+                  );
+                })}
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div className="new-split-add-container">
         <NewSplit />
         <AddSplitBtn />
       </div>
     </>
   );
+
+  // return (
+  //   <>
+  //     <div className="main-container">
+  //       <div className={`${isModalOpen ? 'blurred' : ''}`}>
+  //         <p className="choose">Choose a Workout Split</p>
+  //         <div className="workout-grid">
+  //           {splits.length > 0 &&
+  //             splits.map((el) => {
+  //               return (
+  //                 <ul
+  //                   key={el.split_id}
+  //                   onClick={isModalOpen ? null : () => changeRoute(el.split_id)}
+  //                   className="workout-container"
+  //                 >
+  //                   <div className="image-and-delete-container">
+  //                     <img className="workout-image" src={calendar} alt="Workout"></img>
+  //                     <button onClick={(e) => handleDelete(e, el.split_id)} className="delete-split">
+  //                       Delete
+  //                     </button>
+  //                   </div>
+  //                   <div className="workout-card">
+  //                     <li className="workout-card-title">{el.split_name}</li>
+  //                     <li>{el.days} day split:</li>
+  //                     {el.array_agg.map((name) => {
+  //                       return <li> - {name} day</li>;
+  //                     })}
+  //                     <li>--------------------------------</li>
+  //                     <li>Created on: {el.date.slice(0, 10)}</li>
+  //                   </div>
+  //                 </ul>
+  //               );
+  //             })}
+  //         </div>
+  //       </div>
+  //     </div>
+  //     <div className="new-split-add-container">
+  //       <NewSplit />
+  //       <AddSplitBtn />
+  //     </div>
+  //   </>
+  // );
 };
 
 export default WorkoutSplitGrid;
