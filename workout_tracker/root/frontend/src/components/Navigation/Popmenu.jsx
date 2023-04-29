9443902b6828c4ca4b1afdbed1382dc017e9207c
Marko1310 <marko.cabo13@gmail.com>
2023-04-29 18:46:53 +0200

    adding more functionality to edit menu, close when clicking outside of the menu, also for popmenu

--- workout_tracker/root/frontend/src/components/Navigation/Popmenu.jsx
+++ workout_tracker/root/frontend/src/components/Navigation/Popmenu.jsx
@@ -1,4 +1,5 @@
 //React
+import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 
 // css
@@ -14,13 +15,26 @@ import { GlobalContext } from '../../context/GlobalContext';
 const Popmenu = ({ isMenuOpen, setIsMenuOpen, handleLogout }) => {
   const { user } = useContext(GlobalContext);
 
+  const popMenuRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (popMenuRef.current && !popMenuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [setIsMenuOpen]);
+
   return (
-    <div className={`popmenu ${isMenuOpen ? 'open' : 'close'}`}>
+    <div className={`popmenu ${isMenuOpen ? 'open' : 'close'}`} ref={popMenuRef}>
       <div className="x-container">
         <p className="navigation-user black">{user.email}</p>
         <img className="x" alt="x" src={x} onClick={() => setIsMenuOpen(false)} />
       </div>
-
       <div className="popmenu-tags-container">
         <NavLink className="popmenu-tags" to="/splits" onClick={() => setIsMenuOpen(false)}>
           Home
--- workout_tracker/root/frontend/src/components/Workouts/EditMenu.jsx
+++ workout_tracker/root/frontend/src/components/Workouts/EditMenu.jsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useRef } from 'react';
+
 // css
 import './EditMenu.css';
 
@@ -13,19 +15,33 @@ const EditMenu = ({
   exercise,
   handleHistoryOpen,
 }) => {
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsEditMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, [setIsEditMenuOpen]);
+
   return (
     isEditMenuOpen && (
-      <div className="editMenu">
+      <div className={`editMenu ${isEditMenuOpen ? 'open' : ''}`} ref={menuRef}>
         <div className="x-edit-container">
           <img className="x-edit" alt="x" src={x} onClick={() => setIsEditMenuOpen(false)} />
         </div>
 
         <div className="edit-tags-container">
           <p onClick={(e) => handleHistoryOpen(exercise)} className="history-exercise">
-            History
+            - View exercise history
           </p>
           <p onClick={(e) => handleDeleteExercise(e, id, exercise_id)} className="delete-exercise">
-            Delete
+            - Delete exercise
           </p>
         </div>
       </div>
