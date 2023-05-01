6515d6fb59f092fdc65b6ab5e0f49353434c276f
Marko1310 <marko.cabo13@gmail.com>
2023-05-01 23:21:13 +0200

    changed port 3000 on server

--- workout_tracker/root/frontend/src/services/Api/api.js
+++ workout_tracker/root/frontend/src/services/Api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
 // const API_URL = "https://workouttracker-server.onrender.com";
+const API_URL = 'https://delightful-attire-moth.cyclic.app';
 
 export default axios.create({
   baseURL: API_URL,
