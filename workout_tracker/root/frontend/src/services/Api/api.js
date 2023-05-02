4a5c0eccc5cbca00b86f1c3ed74ef329bf5d25e7
Marko1310 <marko.cabo13@gmail.com>
2023-05-02 15:02:57 +0200

    changes for deployment

--- workout_tracker/root/frontend/src/services/Api/api.js
+++ workout_tracker/root/frontend/src/services/Api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8000';
-const API_URL = 'https://workouttracker-api.onrender.com';
+const API_URL = 'https://workouttracker-api-gjbo.onrender.com';
 
 export default axios.create({
   baseURL: API_URL,
