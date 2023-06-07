012c0ac0081dc54a7def36101d358d4289e32b8b
Marko1310 <marko.cabo13@gmail.com>
2023-06-07 10:03:14 +0200

    editWorkoutController completed

--- workout_tracker/root/backend/routes/EditWorkouts.js
+++ workout_tracker/root/backend/routes/EditWorkouts.js
@@ -1,115 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const requiresAuth = require('../middleware/permission');
 
-// services
-const editDataService = require('../services/editDataService');
-const checkDatabaseService = require('../services/checkDatabaseService');
+//controllers
+const editWorkoutsController = require('../controllers/editWorkoutsController');
+
+//middleware
+const requiresAuth = require('../middleware/permission');
 
 //      EDITING DATA     //
 ///////////////////////////////
 // @route   DELETE /api/split/delete
 // @desc    Delete split
 // @access  Private
-router.delete('/split/delete', requiresAuth, async (req, res) => {
-  try {
-    user_id = req.user.id;
-    const { split_id } = req.body;
-
-    const isValidSplitId = await checkDatabaseService.checkSplitId(split_id, user_id);
-    if (isValidSplitId === 0) {
-      return res.status(400).send('Unathorized');
-    }
-
-    const deleteSplit = await editDataService.deleteSplit(split_id, user_id);
-    res.json(deleteSplit.rows);
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
+router.delete('/split/delete', requiresAuth, editWorkoutsController.deleteSplit);
 
 // @route   DELETE /api/split/workout/delete
 // @desc    Delete workout in a split
 // @access  Private
-router.delete('/split/workout/delete', requiresAuth, async (req, res) => {
-  try {
-    user_id = req.user.id;
-    const { split_id, workout_id } = req.body;
-
-    const isValidSplitId = await checkDatabaseService.checkSplitId(split_id, user_id);
-    const isValidWorkoutId = await checkDatabaseService.checkWorkoutId(workout_id, user_id);
-    if (isValidSplitId === 0 || isValidWorkoutId === 0) {
-      return res.status(400).send('Unathorized');
-    }
-
-    const deletedWorkout = await editDataService.deleteWorkout(workout_id, user_id);
-    res.json(deletedWorkout.rows);
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
+router.delete('/split/workout/delete', requiresAuth, editWorkoutsController.deleteWorkout);
 
 // @route   DELETE /api/split/workout/exercise/delete
 // @desc    Delete exercise in a workout
 // @access  Private
-router.delete('/split/workout/exercise/delete', requiresAuth, async (req, res) => {
-  try {
-    user_id = req.user.id;
-    const { workout_id, exercise_id } = req.body;
-
-    const isValidWorkoutId = await checkDatabaseService.checkWorkoutId(workout_id, user_id);
-    const isValidExerciseId = await checkDatabaseService.checkExerciseId(exercise_id, user_id);
-    if (isValidWorkoutId === 0 || isValidExerciseId === 0) {
-      return res.status(400).send('Unathorized');
-    }
-
-    const deleteExercise = await editDataService.deleteExercise(user_id, exercise_id);
-    res.json(deleteExercise.rows);
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
+router.delete('/split/workout/exercise/delete', requiresAuth, editWorkoutsController.deleteExercise);
 
 // @route   DELETE /api/split/workout/exercise/set/delete
 // @desc    Delete set in exercise
 // @access  Private
-router.delete('/split/workout/exercise/set/delete', requiresAuth, async (req, res) => {
-  try {
-    user_id = req.user.id;
-    const { workout_id, exercise_id, track_id } = req.body;
-
-    const isValidWorkoutId = await checkDatabaseService.checkWorkoutId(workout_id, user_id);
-    const isValidExerciseId = await checkDatabaseService.checkExerciseId(exercise_id, user_id);
-    const isValidTrackId = await checkDatabaseService.checkTrackId(exercise_id, track_id, user_id);
-    if (isValidWorkoutId === 0 || isValidExerciseId === 0 || isValidTrackId === 0) {
-      return res.status(400).send('Unathorized');
-    }
-
-    const deleteTrack = await editDataService.deleteTrack(exercise_id, track_id, user_id);
-    res.json(deleteTrack.rows);
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
+router.delete('/split/workout/exercise/set/delete', requiresAuth, editWorkoutsController.deleteSet);
 
 // @route   POST /api/split/workout/editDay
 // @desc    update workout day
 // @access  Private
-router.post('/split/workout/editDay', requiresAuth, async (req, res) => {
-  try {
-    user_id = req.user.id;
-    const { workout_id } = req.body;
-
-    const isValidWorkoutId = await checkDatabaseService.checkWorkoutId(workout_id, user_id);
-    if (isValidWorkoutId === 0) {
-      return res.status(400).send('Unathorized');
-    }
-
-    const updateWorkoutDay = await editDataService.updateWorkoutDay(workout_id);
-    res.json(updateWorkoutDay.rows);
-  } catch (err) {
-    return res.status(500).send(err.message);
-  }
-});
+router.post('/split/workout/editDay', requiresAuth, editWorkoutsController.updateWorkoutDay);
 
 module.exports = router;
