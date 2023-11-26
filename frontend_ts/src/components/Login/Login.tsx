6fd63cf6b96292b8824c45ee5e71660dc62c84ab
Marko1310 <marko.cabo13@gmail.com>
2023-11-26 23:15:57 +0100

    Add Login component, export array of images

---
+++ frontend_ts/src/components/Login/Login.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { images } from './images';
+
+function Login() {
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    setBackgroundImage(images[Math.floor(Math.random() * 7)].img);
+  }, []);
+
+  return (
+    <div className="h-screen w-screen grid grid-cols-[1.5fr,1fr] overflow-y-hidden bg-white">
+      <div className="p-0">
+        <img
+          alt=""
+          className="w-full h-full bg-cover bg-center "
+          style={{
+            backgroundImage: `url(${backgroundImage})`
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
