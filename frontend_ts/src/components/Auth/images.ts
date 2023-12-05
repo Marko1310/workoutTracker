e60d1cc11654ebea96b959d77b75afd760ec4a0e
Marko1310 <marko.cabo13@gmail.com>
2023-12-06 00:41:00 +0100

    Change image import

--- frontend_ts/src/components/Auth/images.ts
+++ frontend_ts/src/components/Auth/images.ts
@@ -1,9 +1,9 @@
 export const images = [
-  { img: '../../../public/images/1.jpg' },
-  { img: '../../../public/images/3.jpg' },
-  { img: '../../../public/images/4.jpg' },
-  { img: '../../../public/images/5.jpg' },
-  { img: '../../../public/images/6.jpg' },
-  { img: '../../../public/images/7.jpg' },
-  { img: '../../../public/images/8.jpg' },
+  { img: '../../../images/1.jpg' },
+  { img: '../../../images/3.jpg' },
+  { img: '../../../images/4.jpg' },
+  { img: '../../../images/5.jpg' },
+  { img: '../../../images/6.jpg' },
+  { img: '../../../images/7.jpg' },
+  { img: '../../../images/8.jpg' },
 ];
