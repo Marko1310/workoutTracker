14b2330d320504d5f30264c8396a574ccb295398
Marko1310 <marko.cabo13@gmail.com>
2023-12-09 15:03:35 +0100

    Add logout functionality to sidebar

--- frontend_ts/src/components/Application/Sidebar.tsx
+++ frontend_ts/src/components/Application/Sidebar.tsx
@@ -1,7 +1,18 @@
+import { SyntheticEvent } from 'react';
+import { useAuth } from '../../context/AuthContext';
+
 function Sidebar() {
+  const { logout } = useAuth()!;
+
+  const handleLogout = (e: SyntheticEvent) => {
+    e.preventDefault();
+    logout();
+  };
+
   return (
-    <div className='hidden h-screen border border-red-700 md:block md:w-60'>
-      Sidebar
+    <div className='hidden h-screen flex-col border border-red-700 md:block md:w-60'>
+      <div>Sidebar</div>
+      <button onClick={handleLogout}>Logout</button>{' '}
     </div>
   );
 }
