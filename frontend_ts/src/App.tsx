79571206d5ae75f3f971e1949561ea348f244c99
Marko1310 <marko.cabo13@gmail.com>
2023-12-12 23:46:04 +0100

    Remove queery default options

--- frontend_ts/src/App.tsx
+++ frontend_ts/src/App.tsx
@@ -6,13 +6,7 @@ import Protected from './pages/Protected';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryCLient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
-  },
-});
+const queryCLient = new QueryClient();
 
 function App() {
   return (
