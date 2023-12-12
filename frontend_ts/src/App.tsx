2d75dc0910e213c917f515438a50bc9ab76e9ce7
Marko1310 <marko.cabo13@gmail.com>
2023-12-12 01:22:51 +0100

    Install react query

--- frontend_ts/src/App.tsx
+++ frontend_ts/src/App.tsx
@@ -3,28 +3,41 @@ import Auth from './pages/Auth';
 import Application from './pages/Application';
 import { AuthProvider } from './context/AuthContext';
 import Protected from './pages/Protected';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryCLient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='' element={<Auth />} />
-          <Route
-            path='app'
-            element={
-              <Protected>
-                <Application />
-              </Protected>
-            }
-          >
-            <Route index path='home' element={<p>Home</p>} />
-            <Route path='dashboard' element={<p>Dashboard</p>} />
-            <Route path='routines' element={<p>Routines</p>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <QueryClientProvider client={queryCLient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='' element={<Auth />} />
+            <Route
+              path='app'
+              element={
+                <Protected>
+                  <Application />
+                </Protected>
+              }
+            >
+              <Route index path='home' element={<p>Home</p>} />
+              <Route path='dashboard' element={<p>Dashboard</p>} />
+              <Route path='routines' element={<p>Routines</p>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
