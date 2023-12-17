057fab006053e3e08410150718cb67dfa74d4b1c
Marko1310 <marko.cabo13@gmail.com>
2023-12-17 22:38:38 +0100

    Add get workouts from current program to service

--- frontend_ts/src/services/workoutServices.ts
+++ frontend_ts/src/services/workoutServices.ts
@@ -1,14 +1,22 @@
 import { workout_api } from './api_config';
 
-const getAllWorkouts = async (id: number) => {
-  return await workout_api.get(`workouts/${id}`);
+const getAllWorkouts = async (userId: number) => {
+  return await workout_api.get(`workouts/${userId}`);
 };
 
-const getCurrentProgram = async (id: number) => {
-  return await workout_api.get(`workout-splits/currentWorkoutSplit/${id}`);
+const getCurrentProgram = async (userId: number) => {
+  return await workout_api.get(`workout-splits/currentWorkoutSplit/${userId}`);
+};
+
+const getWorkoutsFromCurrentProgram = async (
+  userId: number,
+  programId: number,
+) => {
+  return await workout_api.get(`workout-splits/${userId}/${programId}`);
 };
 
 export default {
   getAllWorkouts,
   getCurrentProgram,
+  getWorkoutsFromCurrentProgram,
 };
