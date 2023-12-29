f5ae82af89d39e3bf8e660f836c4ca47d6015d4f
Marko1310 <marko.cabo13@gmail.com>
2023-12-29 13:22:38 +0100

    New program module style, change style of base modal

--- frontend_ts/src/components/Programs/Modal.tsx
+++ frontend_ts/src/components/Programs/Modal.tsx
@@ -9,9 +9,17 @@ const Modal = forwardRef<HTMLDialogElement, Props>(({ children }, ref) => {
   const { closeModal } = useModal(ref as React.RefObject<HTMLDialogElement>);
 
   return (
-    <dialog ref={ref}>
+    <dialog
+      className='relative rounded-md pt-6 backdrop:bg-black/20 backdrop:backdrop-blur-sm'
+      ref={ref}
+    >
+      <button
+        className='absolute right-4 top-2 text-lg font-bold text-red-600 ease-out hover:cursor-pointer'
+        onClick={closeModal}
+      >
+        X
+      </button>
       {children}
-      <button onClick={closeModal}>Close</button>
     </dialog>
   );
 });
--- frontend_ts/src/ui/Workouts/NewProgramModal.tsx
+++ frontend_ts/src/ui/Workouts/NewProgramModal.tsx
@@ -1,10 +1,59 @@
+import { useForm } from 'react-hook-form';
+
 function NewProgramModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <>
-      <div>Create new Program</div>
-      <div>Title</div>
-      <div>Number of days</div>
-    </>
+    <form
+      className='flex w-96 flex-col gap-4 p-4'
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h1 className='text-center text-xl font-bold uppercase'>
+        Add new Program
+      </h1>
+      <div className='flex flex-col gap-1'>
+        <label>Program Title</label>
+        <input
+          placeholder='e.g. Push / Pull / Legs'
+          className='h-14 w-full rounded-md border-[1px] border-neutral-400 pl-2 font-montserrat text-lg font-light transition-all focus-within:border-2 hover:border-2 hover:border-neutral-400 focus:border-neutral-600 focus:outline-none'
+          {...register('program_title', { required: true })}
+        />
+        {errors.program_title && (
+          <p className='text-red-500'>Title is required</p>
+        )}
+      </div>
+
+      <div className='flex flex-col gap-1'>
+        <label>Days per week</label>
+        <select
+          defaultValue=''
+          className='h-14 w-full rounded-md border-[1px] border-neutral-400 pl-2 focus-within:border-2 hover:border-2 hover:border-neutral-400 focus:border-neutral-600 focus:outline-none'
+          {...(register('days'), { required: true })}
+        >
+          <option value='' disabled></option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+          <option value='6'>6</option>
+          <option value='7'>7</option>
+        </select>
+        {errors.days && <p className='text-red-500'>Title is required</p>}
+      </div>
+
+      <button
+        className='w-full rounded-md bg-orange-300 p-3 transition-all hover:bg-orange-400'
+        type='submit'
+      >
+        Add Program
+      </button>
+    </form>
   );
 }
 export default NewProgramModal;
